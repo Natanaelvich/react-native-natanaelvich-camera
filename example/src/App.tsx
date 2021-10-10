0641d7cd1e341a5d227433f90964808727b6fd2f
Natanael Lima <taelima1997@gmail.com>
2021-10-09 23:22:27 -0300

    add example implementation open picker image

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-natanaelvich-camera';
+import { openPickerImage } from 'react-native-natanaelvich-camera';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
   React.useEffect(() => {
-    multiply(3, 7).then(setResult);
+    openPickerImage();
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>CAMERA</Text>
     </View>
   );
 }
